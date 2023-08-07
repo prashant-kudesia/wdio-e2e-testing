c83ae532735b5969cd858fc12db2d6522f304362
prashant kudesia <prashantkudesia946@gmail.com>
2023-08-07 17:56:39 +0530

    adding till Headless

--- test/features/step-definitions/given.ts
+++ test/features/step-definitions/given.ts
@@ -1,5 +1,5 @@
 import { Given } from "@wdio/cucumber-framework"
-import chai = require("chai")
+// import chai = require("chai")
 
 Given(/^Login to inventory web app$/, async function () {
     /**
@@ -8,20 +8,24 @@ Given(/^Login to inventory web app$/, async function () {
      * 2.login function
      */
     await browser.url("https://www.saucedemo.com")
+
+    console.log(`>> Test Username:${process.env.TEST_USERNAME}`)
     // await browser.setTimeout({ implicit: 15000, pageLoad: 10000 })
     await browser.maximizeWindow()
 
+    // await (await $(`//*[@id="user-name"]`)).setValue(process.env.TEST_STD_USERNAME);
+
     try {
-        await (await $(`//*[@id="user-name"]`)).setValue("standard_user");
-        await (await $(`//*[@id="password"]`)).setValue("secret_sauce");
+        await (await $(`//*[@id="user-nam"]`)).setValue(process.env.TEST_STD_USERNAME!);
+        await (await $(`//*[@id="password"]`)).setValue(process.env.TEST_STD_PASSWORD!);
         await (await $(`//*[@id="login-button"]`)).click();
     } catch (err) {
         console.log(`Error in first login. Retrying...`)
 
         await browser.pause(2000)
         await browser.refresh()
-        await (await $(`//*[@id="user-name"]`)).setValue("standard_user");
-        await (await $(`//*[@id="password"]`)).setValue("secret_sauce");
+        await (await $(`//*[@id="user-name"]`)).setValue(process.env.TEST_STD_USERNAME!);
+        await (await $(`//*[@id="password"]`)).setValue(process.env.TEST_STD_PASSWORD!);
         await (await $(`//*[@id="login-button"]`)).click();
 
 
@@ -41,6 +45,6 @@ Given(/^Login to inventory web app$/, async function () {
     // await (await $(`//*[@id="password"]`)).setValue("secret_sauce");
     // await (await $(`//*[@id="login-button"]`)).click();
 
-    await browser.debug()
+    // await browser.debug()
 
 })
